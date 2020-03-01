bfc302d8ab736c933c48f6ac9238b6215bf9554e
Freek3r <1163254784@qq.com>
2020-03-01 23:11:07 +0800

    更改数据库配置；添加存放静态文件的文件夹(static file)

--- SCUCTF_CMS/settings.py
+++ SCUCTF_CMS/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SCUCTF_CMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'USERNAME',
+        'PASSWORD': 'PASSWORD',
+        'NAME': 'scu_ctf',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'clollected_static')
