85b383215536e713fe87f0e138a51e1f9f1c7741
Freek3r <1163254784@qq.com>
2020-03-02 21:48:20 +0800

    完成index页面，根据用户是否已经登录来渲染模板

--- CTF/views.py
+++ CTF/views.py
@@ -6,8 +6,17 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-# TODO: 根据是否已经登录返回 有或者没有 登录注册 按钮的header
 def index(request):
+    """
+    首页显示，如果已经登录，就显示个人中心，不显示登录和注册按钮
+    如果未登录，就显示登录和注册按钮，不显示个人中心
+    :param request: 用户请求
+    :return: 返回渲染后页面
+    """
+    if request.user.is_authenticated():
+        return render(request, 'index.html',  {
+            'user': request.user
+        })
     return render(request, 'index.html')
 
 
