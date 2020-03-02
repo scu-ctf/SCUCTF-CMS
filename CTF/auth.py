c6f1e21e827b89a93fe90b7b80dc08879a5ded54
Freek3r <1163254784@qq.com>
2020-03-02 21:30:07 +0800

    添加了参数数据类型和函数返回值的数据类型

--- CTF/auth.py
+++ CTF/auth.py
@@ -1,35 +1,35 @@
-from .models import MyUser
+from .models import User
 from time import time
 from django.contrib.auth import authenticate, login, logout
 
 
-def create_user(username, password, email):
+def create_user(username: str, password: str, email: str) -> bool:
     """
     注册普通用户
     :param username: 用户名
     :param password: 密码
     :param email: 邮件
-    :return: 成功返回true
+    :return: 成功返回true/false
     """
     try:
-        MyUser.objects.create_user(username=username, password=password, email=email, points=0, admin_level=0,
-                                   is_scuer=False, email_verified=False, register_time=time()).save()
+        User.objects.create_user(username=username, password=password, email=email, points=0, admin_level=0,
+                                 is_scuer=False, email_verified=False, register_time=time()).save()
         return True
     except:
         return False
 
 
-def create_superuser(username, password, email):
+def create_superuser(username: str, password: str, email: str) -> bool:
     """
     注册管理员用户
     :param username: 用户名
     :param password: 密码
     :param email: 邮件
-    :return: 成功返回true
+    :return: 成功返回true/false
     """
     try:
-        MyUser.objects.create_user(username=username, password=password, email=email, points=0, is_superuser=True,
-                                   admin_level=1, is_scuer=False, email_verified=False, register_time=time()).save()
+        User.objects.create_user(username=username, password=password, email=email, points=0, is_superuser=True,
+                                 admin_level=1, is_scuer=False, email_verified=False, register_time=time()).save()
         return True
     except:
         return False
