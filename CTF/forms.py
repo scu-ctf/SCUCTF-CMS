ca955b8449e4e16f60ba3e05c1f41a89c5a186a7
IzaiahSun <sunyq9934@foxmail.com>
2020-03-03 19:12:30 +0800

    更新了login页面

--- CTF/forms.py
+++ CTF/forms.py
@@ -11,5 +11,6 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="用户名", max_length=150)
+    username = forms.CharField(label="用户名", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=32, widget=forms.PasswordInput)
+    remember = forms.BooleanField(label="记住我", widget=forms.CheckboxInput(attrs={'class': 'form-check-label'}))
