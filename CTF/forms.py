00c53ed389d3f28e511e25392d968b6af6ac9f37
Freek3r <1163254784@qq.com>
2020-03-03 11:32:30 +0800

    规范代码格式(pycharm报红看着太难受了)

--- CTF/forms.py
+++ CTF/forms.py
@@ -3,10 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 from .models import User
 
+
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("username", "email",)
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=150)
