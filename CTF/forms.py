54e8e6434e597c56c2ae712556f7a25fb09ec722
IzaiahSun <sunyq9934@foxmail.com>
2020-03-04 12:33:16 +0800

    完善了找回密码页面

--- CTF/forms.py
+++ CTF/forms.py
@@ -14,3 +14,12 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=32, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     remember = forms.BooleanField(label="记住我", widget=forms.CheckboxInput(attrs={'class': 'form-check-label'}))
+
+
+class ForgetPasswordStep1(forms.Form):
+    email = forms.EmailField(label="邮箱", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+
+class ForgetPasswordStep2(forms.Form):
+    password = forms.CharField(label="密码", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    repeat_password = forms.CharField(label="重复密码", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
---
+++ CTF/static/js/navigator.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $(".topBar .topBarLinks #homeLink").mouseenter(function() {
+        $(this).animate({
+            backgroundColor: "#f0f2f5"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #homeLink").mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#ffffff"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #newsLink").mouseenter(function() {
+        $(this).animate({
+            backgroundColor: "#f0f2f5"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #newsLink").mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#ffffff"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #challengesLink").mouseenter(function() {
+        $(this).animate({
+            backgroundColor: "#f0f2f5"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #challengesLink").mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#ffffff"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #toolsLink").mouseenter(function() {
+        $(this).animate({
+            backgroundColor: "#f0f2f5"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #toolsLink").mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#ffffff"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #aboutLink").mouseenter(function() {
+        $(this).animate({
+            backgroundColor: "#f0f2f5"
+        }, 500);
+    });
+    $(".topBar .topBarLinks #aboutLink").mouseleave(function() {
+        $(this).animate({
+            backgroundColor: "#ffffff"
+        }, 500);
+    });
+});
